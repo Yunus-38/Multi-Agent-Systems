99548a8c6a117d5685151658be99a1f779d9b9b7
Yunus-38 <yunusbaki38@gmail.com>
2024-02-16 20:26:37 +0300

    Update LifeCycle.java

--- src/simulation/LifeCycle.java
+++ src/simulation/LifeCycle.java
@@ -1,10 +1,31 @@
 package simulation;
 
 public class LifeCycle {
-
+	
+	//private static MessageBox messageBox;
+	//private static Map map;
+	private static int timeStep =0;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		while(true) {
+			
+			broadcastTasks();
+			
+			timeStep++;
+		}
 
+	}
+	
+	public static void broadcastTasks() {
+		//inform all agents of all tasks one by one.
+	}
+	
+	public static void generateTasks() {
+		//call the generate task method of every cell in map.
+	}
+	
+	public static void moveAgents() {
+		//call the move function of every agent.
 	}
 
 }
